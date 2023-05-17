60a5df4780fc5e95673786a9e5ea603e25893e02
Fulvio Corno <fulvio.corno@polito.it>
2023-05-17 14:27:55 +0200

    soluzione (parziale) punto 2

---
+++ src/main/java/it/polito/tdp/borders/model/Event.java
@@ -0,0 +1,39 @@
+package it.polito.tdp.borders.model;
+
+public class Event implements Comparable<Event>{
+	private int time ;  // passi di simulazione
+	// tipo di evento: INGRESSO (sottinteso)
+	private Country destinazione ;
+	private int dimensione ;
+	
+	public Event(int time, Country destinazione, int dimensione) {
+		super();
+		this.time = time;
+		this.destinazione = destinazione;
+		this.dimensione = dimensione;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public Country getDestinazione() {
+		return destinazione;
+	}
+
+	public int getDimensione() {
+		return dimensione;
+	}
+
+	@Override
+	public String toString() {
+		return "Event [time=" + time + ", destinazione=" + destinazione + ", dimensione=" + dimensione + "]";
+	}
+
+	@Override
+	public int compareTo(Event other) {
+		return this.time-other.time;
+	}
+	
+	
+}
---
+++ src/main/java/it/polito/tdp/borders/model/Simulator.java
@@ -0,0 +1,62 @@
+package it.polito.tdp.borders.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+
+public class Simulator {
+	
+	// Stato del sistema e output
+	private Map<Country, Integer> stanziali ;
+	
+	// Parametri della simulazione
+	private Graph<Country, DefaultEdge> graph ;
+	private int nPersone = 1000 ;
+	private Country partenza ;
+	
+	// Output
+	private int nPassi ;
+	
+	// Coda degli eventi
+	private PriorityQueue<Event> queue ;
+	
+	
+	public Simulator(Graph<Country, DefaultEdge> graph, Country partenza) {
+		this.graph = graph ;
+		this.partenza = partenza ;
+		
+		this.nPassi = 0 ;
+		
+		this.stanziali = new HashMap<Country, Integer>() ;
+		for(Country c: this.graph.vertexSet()) {
+			this.stanziali.put(c, 0) ;
+		}
+		
+		this.queue = new PriorityQueue<>() ;
+	}
+	
+	private void initialize() {
+		this.queue.add(new Event( 0, this.partenza, this.nPersone ));
+	}
+	
+	private void run() {
+		while(!this.queue.isEmpty()) {
+			Event e = this.queue.poll() ;
+			int time = e.getTime() ;
+			Country destinazione = e.getDestinazione() ;
+			int dimensione = e.getDimensione() ;
+			
+			
+			// dimensione / 2 si dividono negli stati adiacenti
+			// generando eventi INGRESSO con la quota di persone
+
+			// i rimanenti diventano stanziali nello stato 'destinazione'
+
+		}
+	}
+	
+
+}
